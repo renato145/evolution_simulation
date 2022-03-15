aa4312de4640c1f6f2b8c74e7f2798621d52bb51
Renato <renato145@hotmail.com>
2022-03-15 15:13:11 +1030

    change shape to rect

--- bevy_version/src/food.rs
+++ bevy_version/src/food.rs
@@ -1,11 +1,8 @@
 use crate::{utils::random_screen_position, Speed};
 use bevy::prelude::*;
-use bevy_prototype_lyon::{
-    prelude::{DrawMode, FillMode, GeometryBuilder},
-    shapes,
-};
 
-const FOOD_SIZE: f32 = 3.0;
+const FOOD_SPAWN_TIME: f32 = 0.5;
+const FOOD_SIZE: f32 = 5.0;
 const FOOD_SPEED_FACTOR: f32 = 1.2;
 const MAX_FOOD_INSTANCES: usize = 20;
 
@@ -13,7 +10,7 @@ pub struct FoodPlugin;
 
 impl Plugin for FoodPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(FoodSpawnTimer(Timer::from_seconds(0.5, true)))
+        app.insert_resource(FoodSpawnTimer(Timer::from_seconds(FOOD_SPAWN_TIME, true)))
             .insert_resource(FoodCount(0))
             .add_system(food_spawn);
     }
@@ -35,20 +32,20 @@ fn food_spawn(
 ) {
     if (food_count.0 < MAX_FOOD_INSTANCES) && timer.0.tick(time.delta()).just_finished() {
         let pos = random_screen_position(windows.get_primary().unwrap());
-        let shape = shapes::Circle {
-            radius: FOOD_SIZE,
+        let shape_bundle = SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgba(0.0, 1.0, 0.0, 0.75),
+                custom_size: Some(Vec2::new(FOOD_SIZE, FOOD_SIZE)),
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(pos.x, pos.y, 0.0),
             ..Default::default()
         };
-        let shape_bundle = GeometryBuilder::build_as(
-            &shape,
-            DrawMode::Fill(FillMode::color(Color::GREEN)),
-            Transform::from_xyz(pos.x, pos.y, 0.0),
-        );
+
         commands
             .spawn_bundle(shape_bundle)
             .insert(Food)
             .insert(Speed::random_direction(FOOD_SPEED_FACTOR));
         food_count.0 += 1;
-        println!("Food count: {}", food_count.0);
     }
 }
